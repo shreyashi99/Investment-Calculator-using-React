b871b216c4d54f5d99f67dc1f39a115d955ddeb8
Shreyashi <shreyasi99rc@gmail.com>
2024-05-16 11:58:41 +0530

    A complete investment calculator app

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import Header from "./components/Header.jsx";
+import Results from "./components/Results.jsx";
+import UserInput from "./components/UserInput.jsx";
+import { useState } from "react";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+
+      {inputIsValid ? (
+        <Results input={userInput} />
+      ) : (
+        <p className="center">Please enter a value greater than zero...</p>
+      )}
+    </>
+  );
+}
+
+export default App;
